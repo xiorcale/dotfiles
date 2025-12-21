beae6e572007fdcf1dfad4cbe4eb989318af96e3
xiorcale <17534323+xiorcale@users.noreply.github.com>
2025-12-21 10:32:48 +0100

    update nvidia install for arch

---
+++ archlinux/aur-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
+
+yay -Y --gendb
+yay -Syu --devel
+yay -Y --devel --save
+
+# nvidia driver
+yay nvidia-580xx-dkms
