d0c50eedb8a64506b69b2620ba860b6797cec2c8
xiorcale <17534323+xiorcale@users.noreply.github.com>
2026-02-19 19:10:44 +0100

    use paru instead of yay

--- archlinux/aur-install.sh
+++ archlinux/aur-install.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-
-sudo pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
 makepkg -si
 
-yay -Y --gendb
-yay -Syu --devel
-yay -Y --devel --save
-
 # nvidia driver
-yay -S linux-headers nvidia-580xx-dkms nvidia-580xx-utils lib32-nvidia-580xx-utils
-yay -S nvidia-580xx-settings
-yay -S opencl-nvidia-580xx lib32-opencl-nvidia-580xx
-yay -S intellij-idea-ultimate-edition
+paru -S linux-headers nvidia-580xx-dkms nvidia-580xx-utils lib32-nvidia-580xx-utils
+paru -S nvidia-580xx-settings
+paru -S opencl-nvidia-580xx lib32-opencl-nvidia-580xx
+paru -S intellij-idea-ultimate-edition
